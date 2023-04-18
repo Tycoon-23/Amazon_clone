ed2c98389b14de6ba233140322d954f9b46076db
Aditya Vyavahare <vvaditya23@icloud.com>
2023-04-18 12:23:45 +0530

    Initial Commit

---
+++ Amazon_clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Amazon_clone
+//
+//  Created by ヴィヤヴャハレ・アディティア on 18/04/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
